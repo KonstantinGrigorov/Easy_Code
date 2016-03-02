4b9a4fae065db5352b0baba5a75b442703d7d3f0
KonstantinGrigorov <K.Grigorov@i.ua>
2016-03-02 12:09:29 +0200

    Added files via upload

---
+++ HT1_reverse_array.php
@@ -0,0 +1,27 @@
+﻿
+
+<?php
+/*1. Написать функцию которая принимает массив и возвращает массив с элементами в обратном порядке. Пример:
+array(1, 2, 3) => array(3, 2, 1)*/
+
+
+
+
+
+ function reverse_array_own($array)
+{
+$elements_count = count($array);
+$result = array();
+for($i = $elements_count; $i >= 1; $i--)
+{
+$result[] = $array[$i - 1];
+}
+return $result;
+}
+
+$array = array(1,2,3,"hello",5, "world");
+reverse_array_own ($array);
+var_dump(reverse_array_own($array));
+print_r($array);
+
+?>
